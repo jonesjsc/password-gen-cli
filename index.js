64a1edeacfaf6ba3214e5ab463f361afdbebfb6b
Jason Jones <jason.e.jones@gmail.com>
2021-08-04 23:42:55 -0400

    integrated clipboardy where the password is automatically placed in the clipboard

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const program = require('commander');
 const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+
 const log = console.log;
 const createPassword = require('./utils/createPassword');
 
@@ -17,5 +19,9 @@ const { length, save, numbers, symbols } = program.opts();
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols);
 
+// Copy to clipboard
+clipboardy.writeSync(generatedPassword);
+
 // Output generated password
 log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
